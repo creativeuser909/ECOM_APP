0817c2990ee6d57c65c7b712eeffdd9654b484bf
Manish Kumar <94158139+creativeuser909@users.noreply.github.com>
2024-04-26 16:40:27 +0000

    Camera Images Uploaded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 function App() {
   return (
     <>
-    <div className="text-3xl">
-      <h1>
-        Hello World!
-      </h1>
-
-    </div>
+      <Header />
+      <main className='min-h-[calc(100vh-120px)]'>
+        <Outlet />
+      </main>
+      <Footer />
     </>
   );
 }
